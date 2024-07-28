3ede5b3a30963503cbced29ece99464764d13b41
ThiCsCosta <thycscosta@gmail.com>
2024-07-28 16:48:36 -0300

    add chamada do db.js

--- API_Breaking/app.js
+++ API_Breaking/app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
-const port = 3000;
 const userRoute = require("./src/routes/user.route");
+const conectDataBase = require("./src/database/db")
 
+const port = 3000;
+
+//Conexão com database
+conectDataBase();
 //Lendo arquivos JSON
 app.use(express.json());
 app.use("/user", userRoute);
